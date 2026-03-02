051d19cd15c58581ad0c42d1c6e48945ab2baef0
Yogesh Kulkarni <yogesh7318@gmail.com>
2026-03-02 11:00:44 +0530

    Modified app.js for testing failure email at 11 AM

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ console.log("Hello from Yogesh");
 console.log("New update");
 console.log("Today is: " + new Date());
 console.log("Testing email");
-throw new Error("Force failure");
+throw new Error("Force failure on gmail");
